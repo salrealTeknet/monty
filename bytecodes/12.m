3f2ad15cb7d0323c42d460b052ea795ba9e783fd
salrealTeknet <salihuabduljawwad.a@gmail.com>
2022-10-06 13:18:34 +0000

    monty

---
+++ bytecodes/12.m
@@ -0,0 +1,8 @@
+push 1
+push 2
+push 3
+pall
+add
+pall
+Footer
+© 2022 GitHub
---
+++ bytecodes/nop.m
@@ -0,0 +1,18 @@
+nop
+push 0 Push 0 onto the stack
+push 1 Push 1 onto the stack
+
+push 2
+  push 3
+                   pall    
+
+
+ur ugly mama
+   nop your mama                        
+push 4
+
+    push 5    
+      push    6        
+
+pall This is the end of our program. Monty is awesome!
+nop
